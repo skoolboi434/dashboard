66229225d006f89918e66abf52fef1b6cf9dfb7b
Travis Davis <skoolboi434@gmail.com>
2022-12-20 12:50:56 -0500

    Dashboard admin menu added

---
+++ dashboard.php
@@ -0,0 +1,25 @@
+
+
+<div class="dashboard-main">
+  <div class="left-side">
+    <ul class="db-menu">
+      <li class="menu-heading">Dashboard</li>
+      <li class="db-menu-item active">
+        <a href="index?page=dashboard" class="db-menu-link"><i class="fa-solid fa-house"></i> Overview</a>
+      </li>
+      <li class="db-menu-item">
+        <a href="index?page=dashboard" class="db-menu-link"><i class="fa-solid fa-thumbtack"></i> Products</a>
+      </li>
+      <li class="db-menu-item">
+        <a href="index?page=dashboard" class="db-menu-link"><i class="fa-solid fa-shop"></i> Orders</a>
+      </li>
+      <li class="db-menu-item">
+        <a href="index?page=dashboard" class="db-menu-link"><i class="fa fa-user"></i> Users</a>
+      </li>
+    </ul>
+  </div>
+  <div class="right-side">
+    <h1>Dashboard</h1>
+  </div>
+</div>
+
